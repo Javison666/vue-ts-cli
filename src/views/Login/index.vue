5e8c37a90c7b3365a2ec125ccfb1cb302f685881
Weiqing Ji <hzez1110@163.com>
2019-05-17 11:31:21 +0800

    update

---
+++ src/views/Login/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="home">
+        <img
+            alt="Vue logo"
+            src="../assets/logo.png"
+        >
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { login } from "@/req/user";
+@Component({
+	components: {
+		HelloWorld,
+	},
+})
+export default class Home extends Vue {
+    public mounted() {
+        login({
+			username: "1234",
+            password: "123",
+            imageCode: "123",
+            smsCode: "123",
+        });
+    }
+}
+</script>
